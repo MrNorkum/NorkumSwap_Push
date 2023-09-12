d797d433194d1cac688aaaa633b3d568b0efa8de
Mr. Norkum <kelestayyib@gmail.com>
2023-09-13 01:07:05 +0300

    Successful

--- source2.c
+++ source2.c
@@ -12,6 +12,21 @@ int	isnumeric(char *str)
 	return (1);
 }
 
+int	my_atoi(t_ps *ps, const char *str, int sign, long res)
+{
+	while ((9 <= *str && *str <= 13) || *str == 32)
+		str++;
+	if (*str == 45)
+		sign = -1;
+	if (*str == 43 || *str == 45)
+		str++;
+	while (48 <= *str && *str <= 57)
+		res = res * 10 + *str++ - 48;
+	if (res * sign < -2147483648 || res * sign > 2147483647)
+		return (free_function(ps), ft_printf("Error\n"), exit(1), 0);
+	return (res * sign);
+}
+
 int	repeat(t_lst *stack, int nbr)
 {
 	if (!stack)
@@ -54,19 +69,4 @@ int	get_middle(int *pivot, t_lst *lst, int len)
 	}
 	ft_sort_int_tab(arr, len);
 	return (*pivot = arr[len / 2], free(arr), 1);
-}
-
-int	my_atoi(t_ps *ps, const char *str, int sign, long res)
-{
-	while ((9 <= *str && *str <= 13) || *str == 32)
-		str++;
-	if (*str == 45)
-		sign = -1;
-	if (*str == 43 || *str == 45)
-		str++;
-	while (48 <= *str && *str <= 57)
-		res = res * 10 + *str++ - 48;
-	if (res * sign < -2147483648 || res * sign > 2147483647)
-		return (free_function(ps), ft_printf("Error\n"), exit(1), 0);
-	return (res * sign);
 }
